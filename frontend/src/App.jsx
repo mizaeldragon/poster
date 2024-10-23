472a0ad85f576bca46da9029c23abab0b210ffbf
mizaeldragon <raidechin@gmail.com>
2024-10-23 10:26:17 -0300

    feat: add const do axios no front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
     fetchPosts();
   }, []);
 
+  axios.defaults.baseURL = "https://poster-we5k.onrender.com";
+
   const fetchPosts = async () => {
     try {
       const response = await axios.get("/posts");
