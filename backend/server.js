a01f0ad0d28d9ed68eb97d180c97fdbf581bb998
mizaeldragon <raidechin@gmail.com>
2024-10-23 04:49:26 -0300

    feat: add variavel de cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200,
 };
