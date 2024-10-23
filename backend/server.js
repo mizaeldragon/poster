fbd4d8fdbdc71005b27944be149f570fddfe1617
mizaeldragon <raidechin@gmail.com>
2024-10-23 10:56:45 -0300

    feat: mudei o host do env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://poster-we5k.onrender.com"],
+  origin: process.env.CORS_ORIGIN || "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200,
 };
