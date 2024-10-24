7af97b53cc54e906244fe37935bf5c736a6c4af4
mizaeldragon <raidechin@gmail.com>
2024-10-24 03:16:03 -0300

    feat: mudei a port no vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT || 5173,
+    port: process.env.PORT || 3000,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
